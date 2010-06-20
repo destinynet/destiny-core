4795e2bfdd7b8f2c9245c2d5b57f4365e853f143
smallufo <smallufo@macminixp>
2010-06-20 23:36:12 +0800

    .

--- src/main/java/destiny/utils/AbstractDao.java
+++ src/main/java/destiny/utils/AbstractDao.java
@@ -1,14 +1,19 @@
 package destiny.utils;
 
 import java.io.Serializable;
+import java.util.List;
 
 public interface AbstractDao<T>
 {
   public T get(Serializable id);
-  
   public T save(T t);
-  
   public T update(T t);
-  
   public void delete(T t);
+  
+  public T flush(T t);
+  public void refresh(T t);
+  
+  public long getCount();
+  public List<T> findAll();
+  public List<T> findAll(int start, int count);
 }
