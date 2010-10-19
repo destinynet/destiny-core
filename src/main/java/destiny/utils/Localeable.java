d0aea505994e6327c090542c27dca601f18ad1b3
smallufo <smallufo@macminixp>
2010-10-19 18:10:36 +0800

    Localeable

---
+++ src/main/java/destiny/utils/Localeable.java
@@ -0,0 +1,13 @@
+/**
+ * @author smallufo
+ * Created on 2010/10/19 at 下午5:47:46
+ */
+package destiny.utils;
+
+import java.util.Locale;
+
+/** 此 class 支援不同的 locale */
+public interface Localeable
+{
+  public Locale getLocale();
+}
