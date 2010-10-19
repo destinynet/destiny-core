08cb4d5f158864bf4b2f5c490b062ab3899c563c
smallufo <smallufo@macminixp>
2010-10-19 12:41:08 +0800

    SearchableI18n

--- src/main/java/destiny/utils/Searchable.java
+++ src/main/java/destiny/utils/Searchable.java
@@ -9,7 +9,10 @@ import java.util.List;
 public interface Searchable<T>
 {
   public void reIndex();
+
+  /** 搜尋關鍵字，若找不到，則傳回 長度為零的 list 。 start 為 0-based */
+  public List<T> search(String keyword , int start , int count);
   
-  /** 搜尋關鍵字，若找不到，則傳回 長度為零的 list */
-  public List<T> search(String keyword);
+  /** 搜尋關鍵字，會傳回幾筆資料 */
+  public int getSearchResultSize(String keyword);
 }
---
+++ src/main/java/destiny/utils/SearchableI18n.java
@@ -0,0 +1,17 @@
+/**
+ * @author smallufo
+ * Created on 2010/10/19 at 下午12:22:23
+ */
+package destiny.utils;
+
+import java.util.List;
+import java.util.Locale;
+
+public interface SearchableI18n<T> extends Searchable<T>
+{
+  /** 搜尋關鍵字，若找不到，則傳回 長度為零的 list 。 start 為 0-based */
+  public List<T> search(String keyword , int start , int count , Locale... locales);
+  
+  /** 搜尋關鍵字，會傳回幾筆資料 */
+  public int getSearchResultSize(String keyword , Locale... locales);
+}
