a64470d6c61724ff95cb0c81b907695fbf3058ad
Huang Teng Shiu <smallufo@gmail.com>
2014-08-03 23:39:40 +0800

    rename all Gisgraphy's impl to txt use fluent-hc (from httpClient 4.3) instead of DefaultHttpClient 4.3.4 promote most lib version to destiny-parent

--- src/main/java/destiny/utils/location/GeocodingIF.java
+++ src/main/java/destiny/utils/location/GeocodingIF.java
@@ -1,13 +1,14 @@
 /** 2009/11/26 上午10:38:15 by smallufo */
 package destiny.utils.location;
 
-import java.io.IOException;
-
 import destiny.utils.Tuple;
+
+import java.io.IOException;
+import java.util.Optional;
 
 /** 從地名尋找經緯度 */
 public interface GeocodingIF
 {
-  public Tuple<Double , Double> getLongLat(String placeName) throws IOException;
+  public Optional<Tuple<Double , Double>> getLongLat(String placeName) throws IOException;
 }
 
