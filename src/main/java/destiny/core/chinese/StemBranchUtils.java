1f60125fb6d435176e19bf68eee7c573adc85524
smallufo <smallufo@gmail.com>
2015-05-15 21:54:16 +0800

    五虎遁月

---
+++ src/main/java/destiny/core/chinese/StemBranchUtils.java
@@ -0,0 +1,14 @@
+/**
+ * Created by smallufo on 2015-05-15.
+ */
+package destiny.core.chinese;
+
+public class StemBranchUtils {
+
+  /**
+   * 五虎遁月
+   */
+  public final static HeavenlyStems getMonthStem(HeavenlyStems yearStem , EarthlyBranches monthBranch) {
+    return HeavenlyStems.getHeavenlyStems((yearStem.getIndex() % 5)*2 + (monthBranch.getIndex() >=2 ? monthBranch.getIndex() : monthBranch.getIndex()+ 12));
+  }
+}
