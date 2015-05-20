ebc3d010bf7dd17432a7fc20ae624fd958cec571
smallufo <smallufo@gmail.com>
2015-05-21 00:51:48 +0800

    OnePalm 上線

--- src/main/java/destiny/core/calendar/eightwords/onePalm/PalmGraphics.java
+++ src/main/java/destiny/core/calendar/eightwords/onePalm/PalmGraphics.java
@@ -82,7 +82,7 @@ public class PalmGraphics {
       int metaW = (int)(cellW*2)-10;
       int metaH = (int)(cellH*2)-10;
       Graphics2D metaFrame = (Graphics2D) g.create((int)cellW+10 , (int)cellH+10 , metaW , metaH);
-      renderMeta(metaFrame , (PalmWithMeta) palm, metaW , metaH);
+      renderMeta(metaFrame , (PalmWithMeta) palm, metaW , metaH , fore);
     }
 
 
@@ -92,32 +92,45 @@ public class PalmGraphics {
   }
 
   /** 繪製 meta data 男女、生日、地點... */
-  private static void renderMeta(Graphics2D g , PalmWithMeta palm , int width , int height) {
+  private static void renderMeta(Graphics2D g , PalmWithMeta palm , int width , int height , Color fore) {
 
     int fontSize = width / 20;
-    int fontH = height / 10;
+    int fontH = height / 12;
 
-    g.setFont(new Font(FontRepository.getFontLiHei() , Font.PLAIN, fontSize));
-    g.drawString(palm.getGender().toString() , 1 , fontH);
+    StringBuilder sb = new StringBuilder();
+    sb.append(Palm.getStar(palm.getYear()).substring(1, 2));
+    sb.append(Palm.getStar(palm.getMonth()).substring(1, 2));
+    sb.append(Palm.getStar(palm.getDay()).substring(1, 2));
+    sb.append(Palm.getStar(palm.getHour()).substring(1, 2));
+
+
+    g.setColor(Color.RED);
+    g.setFont(new Font(FontRepository.getFontLiHei(), Font.PLAIN, fontSize*2));
+    g.drawString(sb.toString(), 1 , fontH);
+
+    g.setColor(fore);
+    g.setFont(new Font(FontRepository.getFontLiHei(), Font.PLAIN, fontSize));
+    g.drawString("性別:" + palm.getGender().toString() , 1 , fontH*2);
     Time t1 = palm.getLmt();
     String lmt = String.format("%04d-%02d-%02d %02d:%02d", t1.getYear(), t1.getMonth(), t1.getDay(), t1.getHour(), t1.getMinute());
-    g.drawString("LMT:" + lmt , 1 , fontH*2);
+    g.drawString("LMT:" + lmt , 1 , fontH*3);
 
     Time t2 = Time.getGMTfromLMT(palm.getLmt() , palm.getLoc());
     String gmt = String.format("%04d-%02d-%02d %02d:%02d", t2.getYear(), t2.getMonth(), t2.getDay(), t2.getHour(), t2.getMinute());
-    g.drawString("GMT:" + gmt , 1 , fontH*3);
+    g.drawString("GMT:" + gmt , 1 , fontH*4);
 
-    g.drawString("農曆：" + palm.getChineseDate() + palm.getHour() + "時" , 1 , fontH * 4);
+    EarthlyBranches hour = palm.getHourImpl().getHour(t1 , palm.getLoc());
+    g.drawString("農曆：" + palm.getChineseDate() + hour + "時" , 1 , fontH * 5);
 
     Location loc = palm.getLoc();
     String locString = String.format("%d%s%d'%.3g\" %d%s%d'%.3g\"", loc.getLongitudeDegree(), loc.getEastWest().toString(Locale.ENGLISH).charAt(0), loc.getLongitudeMinute(), loc.getLongitudeSecond()
       , loc.getLatitudeDegree(), loc.getNorthSouth().toString(Locale.ENGLISH).charAt(0), loc.getLatitudeMinute(), loc.getLatitudeSecond());
-    g.drawString("地點：" + locString , 1 , fontH*5);
+    g.drawString("地點：" + locString , 1 , fontH*6);
 
-    g.drawString("順逆：" + palm.getPositiveImpl().getTitle(Locale.TAIWAN) , 1 , fontH * 6);
-    g.drawString("時辰劃分：" + palm.getHourImpl().getTitle(Locale.TAIWAN) , 1 , fontH * 7);
-    g.drawString("子正是：" + palm.getMidnightImpl().getTitle(Locale.TAIWAN) , 1 , fontH * 8);
-    g.drawString("換日：" + (palm.isChangeDayAfterZi() ? "子初換日" : "子正換日") , 1 , fontH * 9);
+    g.drawString("順逆：" + palm.getPositiveImpl().getTitle(Locale.TAIWAN) , 1 , fontH * 7);
+    g.drawString("時辰劃分：" + palm.getHourImpl().getTitle(Locale.TAIWAN) , 1 , fontH * 8);
+    g.drawString("子正是：" + palm.getMidnightImpl().getTitle(Locale.TAIWAN) , 1 , fontH * 9);
+    g.drawString("換日：" + (palm.isChangeDayAfterZi() ? "子初換日" : "子正換日") , 1 , fontH * 10);
   }
 
   /**
