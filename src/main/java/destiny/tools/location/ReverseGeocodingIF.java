6068ca83ddd0d1b3f652b22f0d790418a9aaae34
smallufo <smallufo@gmail.com>
2016-09-14 18:27:45 +0800

    TimeZoneIF , ReverseGeocodingIF : 新增 default impl method

--- src/main/java/destiny/tools/location/ReverseGeocodingIF.java
+++ src/main/java/destiny/tools/location/ReverseGeocodingIF.java
@@ -1,6 +1,8 @@
 /** 2009/11/27 下午7:20:26 by smallufo */
 package destiny.tools.location;
 
+import destiny.core.calendar.Location;
+
 import java.util.Locale;
 import java.util.Optional;
 
@@ -10,5 +12,13 @@ import java.util.Optional;
 public interface ReverseGeocodingIF {
 
   Optional<String> getNearbyLocation(double lng, double lat, Locale locale);
+
+  default Optional<String> getNearbyLocation(Locale locale ,
+    Location.EastWest ew   , int lngDeg , int lngMin , double lngSec ,
+    Location.NorthSouth nw , int latDeg , int latMin , double latSec) {
+    double lng = (ew == Location.EastWest.EAST ? 1 : -1) * (lngDeg + lngMin/60.0 + lngSec/3600.0);
+    double lat = (nw == Location.NorthSouth.NORTH ? 1 : -1) * (latDeg + latMin/60.0 + latSec/3600.0);
+    return getNearbyLocation(lng , lat , locale);
+  }
 }
 
--- src/main/java/destiny/tools/location/TimeZoneIF.java
+++ src/main/java/destiny/tools/location/TimeZoneIF.java
@@ -1,6 +1,8 @@
 /** 2009/10/21 上午2:44:14 by smallufo */
 package destiny.tools.location;
 
+import destiny.core.calendar.Location;
+
 import java.util.Optional;
 import java.util.TimeZone;
 
@@ -11,5 +13,12 @@ public interface TimeZoneIF
 {
   /** 從經緯度查詢 timezone */
   Optional<TimeZone> getTimeZone(double lng, double lat);
+
+  default Optional<TimeZone> getTimeZone(Location.EastWest ew   , int lngDeg , int lngMin , double lngSec ,
+                                         Location.NorthSouth nw , int latDeg , int latMin , double latSec) {
+    double lng = (ew == Location.EastWest.EAST ? 1 : -1) * (lngDeg + lngMin/60.0 + lngSec/3600.0);
+    double lat = (nw == Location.NorthSouth.NORTH ? 1 : -1) * (latDeg + latMin/60.0 + latSec/3600.0);
+    return getTimeZone(lng , lat);
+  }
 }
 
