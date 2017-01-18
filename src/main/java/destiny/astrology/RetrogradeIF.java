aae2d3b27139511562159853563113e9163fc22e
smallufo <smallufo@gmail.com>
2017-01-19 06:05:43 +0800

    RetrogradeIF / impl => julDate

--- src/main/java/destiny/astrology/RetrogradeIF.java
+++ src/main/java/destiny/astrology/RetrogradeIF.java
@@ -9,8 +9,12 @@ import destiny.core.calendar.Time;
  * 計算星體在黃道帶上 逆行 / Stationary (停滯) 的介面，目前 SwissEph 的實作只支援 Planet , Asteroid , Moon's Node (只有 True Node。 Mean 不會逆行！)
  * SwissEph 內定實作是 RetrogradeImpl
  */
-public interface RetrogradeIF
-{
+public interface RetrogradeIF {
+
   /** 下次停滯的時間為何時 (GMT) */
-  Time getNextStationary(Star star , Time fromGmtTime , boolean isForward);
+  Time getNextStationary(Star star, double fromGmt, boolean isForward);
+
+  default Time getNextStationary(Star star, Time fromGmtTime, boolean isForward) {
+    return getNextStationary(star, fromGmtTime.getGmtJulDay(), isForward);
+  }
 }
