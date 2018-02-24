b25cf13886e0a3939985513fb00674a3079b9d87
smallufo <smallufo@gmail.com>
2018-02-25 04:18:06 +0800

    destiny.iching from j to k

--- src/main/java/destiny/iching/HexagramContentProvider.kt
+++ src/main/java/destiny/iching/HexagramContentProvider.kt
@@ -12,13 +12,13 @@ interface HexagramContentProvider : Descriptive {
   val locales: Collection<Locale>
 
   /** 取得此卦的解釋  */
-  fun getValue(hexagram: IHexagram, locale: Locale): String
+  fun getValue(hexagram: IHexagram, locale: Locale): String?
 
   /**
    * @param lineIndex 1<=lineIndex<=6
    * @return 取得第幾爻的解釋
    */
-  fun getLineValue(hexagram: IHexagram, lineIndex: Int, locale: Locale): String
+  fun getLineValue(hexagram: IHexagram, lineIndex: Int, locale: Locale): String?
 
   /** 額外的註解（ex : 用九、用六） , 如果沒有，則傳回 null  */
   fun getExtraExpression(hexagram: IHexagram, locale: Locale): String?
