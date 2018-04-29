ed93c9cd47dc5c7622b7012d4405437fb930f76d
smallufo <smallufo@gmail.com>
2018-04-29 21:07:20 +0800

    EightWordsContextAdapter from J to K

--- src/main/java/destiny/core/calendar/eightwords/IYearMonth.kt
+++ src/main/java/destiny/core/calendar/eightwords/IYearMonth.kt
@@ -4,6 +4,48 @@
  */
 package destiny.core.calendar.eightwords
 
+import destiny.core.calendar.ILocation
+import destiny.core.calendar.TimeTools
+import destiny.core.chinese.StemBranch
+import java.time.chrono.ChronoLocalDateTime
+
+/** 取得年干支的介面  */
+interface IYear {
+
+  fun getYear(gmtJulDay: Double, loc: ILocation): StemBranch
+
+  /**
+   *
+   * @param lmt 傳入當地手錶時間
+   * @param loc 傳入當地經緯度等資料
+   * @return 年干支（天干地支皆傳回）
+   */
+  fun getYear(lmt: ChronoLocalDateTime<*>, loc: ILocation): StemBranch {
+    val gmtJulDay = TimeTools.getGmtJulDay(lmt, loc)
+    return getYear(gmtJulDay, loc)
+  }
+
+}
+
+/**
+ * 取得月干支的介面
+ */
+interface IMonth {
+
+  fun getMonth(gmtJulDay: Double, location: ILocation): StemBranch
+
+  fun getMonth(lmt: ChronoLocalDateTime<*>, loc: ILocation): StemBranch {
+    val gmtJulDay = TimeTools.getGmtJulDay(lmt, loc)
+    return getMonth(gmtJulDay, loc)
+  }
+
+  /**
+   * 南半球月支是否對沖 , 內定是 '否'
+   */
+  fun setSouthernHemisphereOpposition(value: Boolean)
+}
+
+
 /**
  * 年月應該要一起考慮，所以設計這個 Interface
  */
