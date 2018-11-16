a3574f5b0948e81c7eacfd6e46c9f338ffde4d98
smallufo <smallufo@gmail.com>
2018-11-17 04:48:50 +0800

    try to refactory HexagramNameShort , add temp "2" version add KRepositoryFactoryBean

--- src/main/java/destiny/iching/contentProviders/IHexagramNameShort.kt
+++ src/main/java/destiny/iching/contentProviders/IHexagramNameShort.kt
@@ -2,16 +2,17 @@
 package destiny.iching.contentProviders
 
 import destiny.iching.IHexagram
-
-import java.util.Locale
+import java.util.*
 
 /** 取得「短」的卦名，例如「乾」  */
 interface IHexagramNameShort {
 
   /** 取得「短」的卦名，例如「乾」  */
+  @JvmDefault
   fun getNameShort(hexagram: IHexagram, locale: Locale): String
 
   /** 從卦的「短卦名」，反查回 Hexagram  */
+  @JvmDefault
   fun getHexagram(name: String, locale: Locale): IHexagram
 
 }
