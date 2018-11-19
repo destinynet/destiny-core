ac3046d3763cde76d93a044934783c6892515a14
smallufo <smallufo@gmail.com>
2018-11-19 19:56:06 +0800

    refactorying

--- src/main/java/destiny/iching/contentProviders/IExpression.kt
+++ src/main/java/destiny/iching/contentProviders/IExpression.kt
@@ -7,17 +7,4 @@ package destiny.iching.contentProviders
 /**
  * 卦 或 爻 的辭
  */
-interface IExpression : IHexagramExpression , ILineExpression  {
-
-//  /** 取得卦辭  */
-//  fun getHexagramExpression(hexagram: IHexagram, locale: Locale): String
-//
-//  /**
-//   * @param lineIndex 0~7 (0 的話則為針對卦辭)
-//   * @return 取得爻辭
-//   */
-//  fun getLineExpression(hexagram: IHexagram, lineIndex: Int, locale: Locale): String
-//
-//  /** 取得 用六/用九 的爻辭  */
-//  fun getExtraExpression(hexagram: IHexagram, locale: Locale): String?
-}
+interface IExpression : IHexagramExpression , ILineExpression
