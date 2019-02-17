a872ad260f3a8691b0223973221b15964b8c60da
smallufo <smallufo@gmail.com>
2019-02-17 10:48:27 +0800

    destiny-ee from J to K

--- src/main/java/destiny/tools/IsoTool.kt
+++ src/main/java/destiny/tools/IsoTool.kt
@@ -11,10 +11,10 @@ class IsoTool : Serializable {
   companion object {
 
     /** 小寫 2-letter 英文字母 , 例如 'zh'  */
-    private val ISO_LANGUAGES = ImmutableSet.copyOf(Locale.getISOLanguages())
+    private val ISO_LANGUAGES: Set<String> = Locale.getISOLanguages().toSet()
 
     /** 大寫 2-letter 英文字母 , 例如 'TW'  */
-    private val ISO_COUNTRIES = ImmutableSet.copyOf(Locale.getISOCountries())
+    private val ISO_COUNTRIES: Set<String> = Locale.getISOCountries().toSet()
 
     /** 大小寫通吃，即使傳入 'ZH' 也會轉成 'zh' ，並且傳回 true  */
     fun isValidLanguage(s: String): Boolean {
