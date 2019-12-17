223dc1b97824b4891cd85da9b7e9f6af25935ee4
smallufo <smallufo@gmail.com>
2019-12-18 02:49:28 +0800

    IStaticMap

---
+++ src/main/java/destiny/tools/location/IStaticMap.kt
@@ -0,0 +1,29 @@
+/**
+ * Created by smallufo on 2019-12-18.
+ */
+package destiny.tools.location
+
+import destiny.core.calendar.ILatLng
+import java.awt.Color
+import java.util.*
+
+interface IStaticMap {
+
+  enum class MapType {
+    roadmap, satellite, hybrid, terrain
+  }
+
+  data class Marker(val latLng: ILatLng,
+                    val color: Color = Color.decode("#EA4335"),
+                    val label: Char? = null)
+
+  /**
+   * return PNG byte array
+   * @param center 緯度、經度
+   * */
+  suspend fun getImage(center: ILatLng,
+                       width: Int = 600, height: Int = 600,
+                       mapType: MapType = MapType.roadmap,
+                       locale: Locale = Locale.getDefault() ,
+                       markers : List<Marker> = emptyList()): ByteArray
+}
