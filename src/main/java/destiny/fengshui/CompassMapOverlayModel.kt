ad907f1dd0e6fe5c18a0e5504039c11d04bdcf97
smallufo <smallufo@gmail.com>
2020-01-12 22:52:35 +0800

    @param rotate 定義為「0 => 上方為子、下方為午」 , 與地平方位角 同義

--- src/main/java/destiny/fengshui/CompassMapOverlayModel.kt
+++ src/main/java/destiny/fengshui/CompassMapOverlayModel.kt
@@ -11,7 +11,7 @@ data class CompassMapOverlayModel(
   val timeLoc : TimeLoc,
   val place : String? = null,
   val width: Int = 640,
-  val rotate: Double = 180.0,
+  val rotate: Double = 0.0, // 北方朝上
   val zoom: Int = 15,
   // 不透明度 , 0.0 最透明
   val opaque: Float = 0.4f,
