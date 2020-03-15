4a6ad3e46c7e038bbc7aa3057e1cbd21cef3b9c8
smallufo <smallufo@gmail.com>
2020-03-15 23:57:46 +0800

    StaticMapChart refactory

--- src/main/java/destiny/fengshui/CompassMapOverlayModel.kt
+++ src/main/java/destiny/fengshui/CompassMapOverlayModel.kt
@@ -3,23 +3,63 @@
  */
 package destiny.fengshui
 
+import destiny.astrology.Planet
 import destiny.core.ITimeLoc
 import destiny.core.TimeLoc
+import destiny.tools.location.IStaticMap
+import java.io.Serializable
 
+interface ICompassMapOverlayModel : ITimeLoc, Serializable {
+  val timeLoc: TimeLoc
+  val place: String?
 
-data class CompassMapOverlayModel(
-  val timeLoc: TimeLoc,
-  val place: String? = null,
-  val width: Int = 640,
-  val rotate: Double = 0.0, // 北方朝上
+  val width: Int
+
+  /** 北方朝上 */
+  val rotate: Double
+
   /**
    * 14 : 城鎮
    * 16 : 街道
    * 18 : 建物
    */
-  val zoom: Int = 16,
-  // 不透明度 , 0.0 最透明
-  val opaque: Float = 0.4f,
-  // 放大倍數 1,2 or 4
-  val scale: Int = 1
-) : ITimeLoc by timeLoc
+  val zoom: Int
+
+  /** 不透明度 , 0.0 最透明 */
+  val opaque: Float
+
+  /** 放大倍數 1,2 or 4 (Pay-As-You-Go 不支援 4) */
+  val scale: Int
+
+  /** 內定為道路圖 */
+  val mapType: IStaticMap.MapType
+}
+
+data class CompassMapOverlayModel(
+  override val timeLoc: TimeLoc,
+  override val place: String? = null,
+  override val width: Int = 640,
+  /** 北方朝上 */
+  override val rotate: Double = 0.0,
+  /**
+   * 14 : 城鎮
+   * 16 : 街道
+   * 18 : 建物
+   */
+  override val zoom: Int = 16,
+  /** 不透明度 , 0.0 最透明 */
+  override val opaque: Float = 0.4f,
+  /** 放大倍數 1,2 or 4 */
+  override val scale: Int = 1,
+  /** 內定為道路圖 */
+  override val mapType: IStaticMap.MapType = IStaticMap.MapType.roadmap
+) : ITimeLoc by timeLoc, ICompassMapOverlayModel
+
+interface ICompassPointsMapOverlayModel : ICompassMapOverlayModel {
+  /** 要繪製的行星 */
+  val planets: Set<Planet>
+}
+
+data class CompassPointsMapOverlayModel(val embedded: ICompassMapOverlayModel,
+                                        override val planets: Set<Planet> = emptySet()) :
+  ICompassPointsMapOverlayModel, ICompassMapOverlayModel by embedded
