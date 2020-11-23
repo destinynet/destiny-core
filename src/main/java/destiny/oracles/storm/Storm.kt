e9e5599d4e7cb3e965ccf6c85eab5c317475d89f
smallufo <smallufo@gmail.com>
2020-11-24 02:43:31 +0800

    雷雨籤詩

---
+++ src/main/java/destiny/oracles/storm/Storm.kt
@@ -0,0 +1,41 @@
+/**
+ * Created by smallufo on 2020-11-23.
+ */
+package destiny.oracles.storm
+
+import destiny.core.chinese.Stem
+import java.io.Serializable
+
+
+data class Storm(val index: Int,
+                 /** 天干x2 , 長度固定為 2 */
+                 val stems: List<Stem>,
+                 val level: String,
+                 /** 七言絕句 , 四句話 */
+                 val poem: List<String>,
+                 /** 釋義 */
+                 val poemExplain: String,
+                 /** 聖意 , 三字一組，共八組，24 字 */
+                 val verdicts: List<String>,
+                 /** 解曰 */
+                 val meaning: String,
+                 /** 解籤 , advice */
+                 val advice: String,
+                 /** 故事 */
+                 val stories: Set<String>,
+                 /** 東坡解 , 八句話 , 每句 4 字 */
+                 val dongPo: List<String> ,
+                 /** 碧仙註 , 五言絕句 or 七言絕句 */
+                 val greenNote : List<String>) : Serializable {
+  init {
+    require(stems.size == 2)
+    require(poem.size == 4)
+    require(verdicts.size == 8)
+    require(dongPo.size == 8)
+    require(greenNote.size == 4)
+
+    require(!meaning.contains(' '))
+    require(!advice.contains(' '))
+
+  }
+}
