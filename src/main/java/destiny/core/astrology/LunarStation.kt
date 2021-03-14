1373f43580ed2975f3311369a7d43d27af0ef737
smallufo <smallufo@gmail.com>
2021-03-15 00:30:55 +0800

    二十八宿 -> Animal 獸 mapping

--- src/main/java/destiny/core/astrology/LunarStation.kt
+++ src/main/java/destiny/core/astrology/LunarStation.kt
@@ -10,44 +10,46 @@ import destiny.core.News.EastWest.WEST
 import destiny.core.News.NorthSouth.NORTH
 import destiny.core.News.NorthSouth.SOUTH
 import destiny.core.astrology.Planet.*
+import destiny.core.chinese.Animal
 import destiny.tools.ArrayTools
 import java.util.*
 
 /** 二十八宿 */
 sealed class LunarStation(val news: News,
                           nameKey: String,
+                          val animal: Animal,
                           val planet: Planet) : Star(nameKey, LunarStation::class.java.name), ILoop<LunarStation> {
-  object 角 : LunarStation(EAST, "角", JUPITER)
-  object 亢 : LunarStation(EAST, "亢", VENUS)
-  object 氐 : LunarStation(EAST, "氐", SATURN)
-  object 房 : LunarStation(EAST, "房", SUN)
-  object 心 : LunarStation(EAST, "心", MOON)
-  object 尾 : LunarStation(EAST, "尾", MARS)
-  object 箕 : LunarStation(EAST, "箕", MERCURY)
+  object 角 : LunarStation(EAST, "角", Animal.蛟, JUPITER)
+  object 亢 : LunarStation(EAST, "亢", Animal.龍, VENUS)
+  object 氐 : LunarStation(EAST, "氐", Animal.貉, SATURN)
+  object 房 : LunarStation(EAST, "房", Animal.兔, SUN)
+  object 心 : LunarStation(EAST, "心", Animal.狐, MOON)
+  object 尾 : LunarStation(EAST, "尾", Animal.虎, MARS)
+  object 箕 : LunarStation(EAST, "箕", Animal.豹, MERCURY)
 
-  object 斗 : LunarStation(NORTH, "斗", JUPITER)
-  object 牛 : LunarStation(NORTH, "牛", VENUS)
-  object 女 : LunarStation(NORTH, "女", SATURN)
-  object 虛 : LunarStation(NORTH, "虛", SUN)
-  object 危 : LunarStation(NORTH, "危", MOON)
-  object 室 : LunarStation(NORTH, "室", MARS)
-  object 壁 : LunarStation(NORTH, "壁", MERCURY)
+  object 斗 : LunarStation(NORTH, "斗", Animal.獬, JUPITER)
+  object 牛 : LunarStation(NORTH, "牛", Animal.牛, VENUS)
+  object 女 : LunarStation(NORTH, "女", Animal.蝠, SATURN)
+  object 虛 : LunarStation(NORTH, "虛", Animal.鼠, SUN)
+  object 危 : LunarStation(NORTH, "危", Animal.燕, MOON)
+  object 室 : LunarStation(NORTH, "室", Animal.豬, MARS)
+  object 壁 : LunarStation(NORTH, "壁", Animal.貐, MERCURY)
 
-  object 奎 : LunarStation(WEST, "奎", JUPITER)
-  object 婁 : LunarStation(WEST, "婁", VENUS)
-  object 胃 : LunarStation(WEST, "胃", SATURN)
-  object 昴 : LunarStation(WEST, "昴", SUN)
-  object 畢 : LunarStation(WEST, "畢", MOON)
-  object 觜 : LunarStation(WEST, "觜", MARS)
-  object 參 : LunarStation(WEST, "參", MERCURY)
+  object 奎 : LunarStation(WEST, "奎", Animal.狼, JUPITER)
+  object 婁 : LunarStation(WEST, "婁", Animal.狗, VENUS)
+  object 胃 : LunarStation(WEST, "胃", Animal.雉, SATURN)
+  object 昴 : LunarStation(WEST, "昴", Animal.雞, SUN)
+  object 畢 : LunarStation(WEST, "畢", Animal.烏, MOON)
+  object 觜 : LunarStation(WEST, "觜", Animal.猴, MARS)
+  object 參 : LunarStation(WEST, "參", Animal.猿, MERCURY)
 
-  object 井 : LunarStation(SOUTH, "井", JUPITER)
-  object 鬼 : LunarStation(SOUTH, "鬼", VENUS)
-  object 柳 : LunarStation(SOUTH, "柳", SATURN)
-  object 星 : LunarStation(SOUTH, "星", SUN)
-  object 張 : LunarStation(SOUTH, "張", MOON)
-  object 翼 : LunarStation(SOUTH, "翼", MARS)
-  object 軫 : LunarStation(SOUTH, "軫", MERCURY)
+  object 井 : LunarStation(SOUTH, "井", Animal.犴, JUPITER)
+  object 鬼 : LunarStation(SOUTH, "鬼", Animal.羊, VENUS)
+  object 柳 : LunarStation(SOUTH, "柳", Animal.獐, SATURN)
+  object 星 : LunarStation(SOUTH, "星", Animal.馬, SUN)
+  object 張 : LunarStation(SOUTH, "張", Animal.鹿, MOON)
+  object 翼 : LunarStation(SOUTH, "翼", Animal.蛇, MARS)
+  object 軫 : LunarStation(SOUTH, "軫", Animal.蚓, MERCURY)
 
   override fun next(n: Int): LunarStation {
     val thisIndex = values.indexOf(this)
