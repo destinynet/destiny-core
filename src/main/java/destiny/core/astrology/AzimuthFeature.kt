4d907b5c47af8b2122f08c20fcb2f438d5cbe9c1
smallufo <smallufo@gmail.com>
2021-08-16 02:40:00 +0800

    AzimuthFeature

---
+++ src/main/java/destiny/core/astrology/AzimuthFeature.kt
@@ -0,0 +1,55 @@
+/**
+ * Created by smallufo on 2021-08-16.
+ */
+package destiny.core.astrology
+
+import destiny.core.calendar.GmtJulDay
+import destiny.core.calendar.ILocation
+import destiny.tools.Builder
+import destiny.tools.DestinyMarker
+import destiny.tools.Feature
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AzimuthConfig(@Serializable(with = PointSerializer::class)
+                         val star: Star = Planet.SUN,
+                         val coordinate: Coordinate = Coordinate.ECLIPTIC,
+                         val geoAlt: Double = 0.0,
+                         val temperature: Double = 0.0,
+                         val pressure: Double = 1013.25)
+
+@DestinyMarker
+class AzimuthConfigBuilder : Builder<AzimuthConfig> {
+
+  var star: Star = Planet.SUN
+  var coordinate: Coordinate = Coordinate.ECLIPTIC
+  var geoAlt: Double = 0.0
+  var temperature: Double = 0.0
+  var pressure: Double = 1013.25
+
+  override fun build(): AzimuthConfig {
+    return AzimuthConfig(star, coordinate, geoAlt, temperature, pressure)
+  }
+
+  companion object {
+    fun azimuth(block :AzimuthConfigBuilder.() -> Unit = {} ) : AzimuthConfig {
+      return AzimuthConfigBuilder().apply(block).build()
+    }
+  }
+}
+
+
+class AzimuthFeature(val starPositionImpl: IStarPosition<IStarPos>,
+                     val azimuthImpl : IAzimuthCalculator) : Feature<AzimuthConfig,StarPosWithAzimuth> {
+  override val key: String = "azimuth"
+
+  override val defaultConfig: AzimuthConfig = AzimuthConfig()
+
+  override fun getModel(gmtJulDay: GmtJulDay, loc: ILocation, config: AzimuthConfig): StarPosWithAzimuth {
+    val pos: IStarPos = starPositionImpl.getPosition(config.star, gmtJulDay, loc.lat, loc.lng, config.geoAlt, Centric.GEO, config.coordinate, config.temperature, config.pressure)
+    val azimuth = with(azimuthImpl) {
+      pos.getAzimuth(config.coordinate, gmtJulDay, loc, config.temperature, config.pressure)
+    }
+    return StarPosWithAzimuth(pos, azimuth)
+  }
+}
---
+++ src/test/java/destiny/core/astrology/AzimuthConfigTest.kt
@@ -0,0 +1,31 @@
+/**
+ * Created by smallufo on 2021-08-16.
+ */
+package destiny.core.astrology
+
+import destiny.core.AbstractConfigTest
+import destiny.core.astrology.AzimuthConfigBuilder.Companion.azimuth
+import kotlinx.serialization.KSerializer
+import kotlin.test.assertTrue
+
+internal class AzimuthConfigTest : AbstractConfigTest<AzimuthConfig>() {
+  override val serializer: KSerializer<AzimuthConfig> = AzimuthConfig.serializer()
+
+  override val configByConstructor: AzimuthConfig = AzimuthConfig(Planet.MOON, Coordinate.SIDEREAL , 100.0 , 23.0 , 1000.0)
+  override val configByFunction: AzimuthConfig = azimuth {
+    star = Planet.MOON
+    coordinate = Coordinate.SIDEREAL
+    geoAlt = 100.0
+    temperature = 23.0
+    pressure = 1000.0
+  }
+
+  override val assertion: (String) -> Unit = { raw ->
+    logger.info { raw }
+    assertTrue(raw.contains(""""star":\s*"Planet.MOON"""".toRegex()))
+    assertTrue(raw.contains(""""coordinate":\s*"SIDEREAL""".toRegex()))
+    assertTrue(raw.contains(""""geoAlt":\s*100.0""".toRegex()))
+    assertTrue(raw.contains(""""temperature":\s*23.0""".toRegex()))
+    assertTrue(raw.contains(""""pressure":\s*1000.0""".toRegex()))
+  }
+}
