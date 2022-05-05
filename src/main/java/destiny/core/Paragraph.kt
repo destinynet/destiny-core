e60a16c7fd40e57b5fcc0548f4a8c045a53f851d
smallufo <smallufo@gmail.com>
2022-05-05 11:25:52 +0800

    Paragraph add Hint attribute

--- src/main/java/destiny/core/Paragraph.kt
+++ src/main/java/destiny/core/Paragraph.kt
@@ -2,7 +2,13 @@ package destiny.core
 
 import java.io.Serializable
 
-sealed class Paragraph(open val content: String) : Serializable {
-  data class Normal(override val content: String) : Paragraph(content)
-  data class Scripture(override val content: String) : Paragraph(content)
+enum class Hint {
+  POSITIVE,
+  NEGATIVE,
+  NOTICE
+}
+
+sealed class Paragraph(open val content: String, open val hint: Hint? = null) : Serializable {
+  data class Normal(override val content: String, override val hint: Hint? = null) : Paragraph(content, hint)
+  data class Scripture(override val content: String, override val hint: Hint? = null) : Paragraph(content, hint)
 }
