6ee7cffece1d802b103512d465b6f7f9429d0d7e
smallufo <smallufo@gmail.com>
2022-06-10 02:25:09 +0800

    AbstractSeqBooleanRule

---
+++ src/main/java/destiny/core/chinese/ziwei/rules/ZRule.kt
@@ -0,0 +1,15 @@
+/**
+ * Created by smallufo on 2022-06-07.
+ */
+package destiny.core.chinese.ziwei.rules
+
+import destiny.core.chinese.ziwei.IPlate
+import destiny.core.chinese.ziwei.IZiweiFeature
+import destiny.core.chinese.ziwei.ZiweiConfig
+import java.time.chrono.ChronoLocalDateTime
+
+
+interface ZRule<T> {
+
+  fun IZiweiFeature.test(plate: IPlate, lmt : ChronoLocalDateTime<*>, config: ZiweiConfig): T
+}
--- src/main/java/destiny/core/chinese/ziwei/rules/ZRule4.kt
+++ src/main/java/destiny/core/chinese/ziwei/rules/ZRule4.kt
@@ -1,5 +1,5 @@
 /**
- * Created by smallufo on 2022-06-07.
+ * Created by smallufo on 2022-06-10.
  */
 package destiny.core.chinese.ziwei.rules
 
@@ -10,11 +10,10 @@ import javax.inject.Named
 
 /**
  * 桃花星定義: 天相 巨門 太陰 天同 破軍 貪狼 (廉貪對宮的廉貞)
- * 運限子女宮干飛化忌入大命、且有桃花星在大命
+ * 運限子女宮干飛化忌入本命、且有桃花星在大命
  */
 @Named
-class ZRule3 : AbstractSeqBooleanRule() {
-
+class ZRule4 : AbstractSeqBooleanRule() {
 
   override fun testSection(sectionPlate: IPlate, lmt: ChronoLocalDateTime<*>, config: ZiweiConfig): Boolean {
     return sectionPlate.getHouseDataOf(FlowType.SECTION, House.子女)?.let { houseData ->
@@ -24,12 +23,10 @@ class ZRule3 : AbstractSeqBooleanRule() {
       val zStar = transFourImplMap[config.transFour]!!.getStarOf(sb.stem, ITransFour.Value.忌)
       logger.trace { "${sb.stem} 化忌 = $zStar" }
 
-      val 運限子女宮干飛化忌入大命 = sectionPlate.getHouseDataOf(FlowType.SECTION, zStar) == House.命宮
+      val 運限子女宮干飛化忌入本命 = sectionPlate.getHouseDataOf(FlowType.MAIN, zStar) == House.命宮
       val 桃花星在大限命宮 = pinkyInHouse(sectionPlate, FlowType.SECTION, House.命宮)
 
-      運限子女宮干飛化忌入大命 && 桃花星在大限命宮
+      運限子女宮干飛化忌入本命 && 桃花星在大限命宮
     } ?: false
   }
-
-
 }
