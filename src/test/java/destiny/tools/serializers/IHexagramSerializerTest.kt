a77f8e25a4e94934a98397c01a80046d7a395bc0
smallufo <smallufo@gmail.com>
2022-07-06 23:11:21 +0800

    WizardFinish save BirthLog

--- src/test/java/destiny/tools/serializers/IHexagramSerializerTest.kt
+++ src/test/java/destiny/tools/serializers/IHexagramSerializerTest.kt
@@ -7,8 +7,7 @@ import destiny.core.iching.Hexagram
 import kotlinx.serialization.json.Json
 import kotlin.test.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
-import kotlin.test.fail
+import kotlin.test.assertFailsWith
 
 internal class IHexagramSerializerTest {
 
@@ -18,18 +17,23 @@ internal class IHexagramSerializerTest {
       assertEquals(""""111111"""", raw)
       assertEquals(Hexagram.乾, Json.decodeFromString(IHexagramSerializer, raw))
     }
-
-
   }
+//
+//  @Test
+//  fun testSerializeWithModule() {
+//    val format = Json {
+//      serializersModule = SerializersModule {
+//        contextual(IHexagramSerializer)
+//      }
+//    }
+//    println(format.encodeToString(Hexagram.乾))
+//  }
 
   @Test
   fun testException() {
-    try {
-      Json.decodeFromString(IHexagramSerializer, """"11111x"""")
-      fail()
-    } catch (e: Exception) {
-      assertTrue { e is IllegalArgumentException }
-    }
 
+    assertFailsWith(IllegalArgumentException::class) {
+      Json.decodeFromString(IHexagramSerializer, """"11111x"""")
+    }
   }
 }
