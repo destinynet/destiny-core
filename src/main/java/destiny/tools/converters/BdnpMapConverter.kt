985d463d3b4ea0ed9e12c8e39de050eafca86633
smallufo <smallufo@gmail.com>
2023-01-11 10:59:00 +0800

    BirthDataNamePlacePlainConverter

--- src/main/java/destiny/tools/converters/BdnpMapConverter.kt
+++ src/main/java/destiny/tools/converters/BdnpMapConverter.kt
@@ -8,12 +8,13 @@ import java.io.Serializable
 import javax.inject.Named
 
 
+// encrypted version
 @Named
-class BdnpMapConverter(private val bdnpConverter : IContextMap<IBirthDataNamePlace>) : BdnpConverter, Serializable {
+class BdnpMapConverter(private val birthDataNamePlaceConverter : IContextMap<IBirthDataNamePlace>) : BdnpConverter, Serializable {
 
   override fun getMap(bdnp: IBirthDataNamePlace, additional: Map<String, String>): Map<String, String> {
     return additional.toMutableMap().apply {
-      putAll(bdnpConverter.getMap(bdnp))
+      putAll(birthDataNamePlaceConverter.getMap(bdnp))
     }.toMap()
   }
 }
