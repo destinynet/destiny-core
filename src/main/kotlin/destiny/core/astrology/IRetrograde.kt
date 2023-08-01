4bc58c5e3f823dbec48fac77de0fcfdcded4b5d4
smallufo <smallufo@gmail.com>
2023-08-01 17:38:37 +0800

    IRetro adding features

--- src/main/kotlin/destiny/core/astrology/IRetrograde.kt
+++ src/main/kotlin/destiny/core/astrology/IRetrograde.kt
@@ -3,11 +3,45 @@
  */
 package destiny.core.astrology
 
+import destiny.core.astrology.Centric.GEO
+import destiny.core.astrology.Coordinate.ECLIPTIC
+import destiny.core.astrology.RetrogradePhase.*
+import destiny.core.astrology.StationaryType.DIRECT_TO_RETROGRADE
+import destiny.core.astrology.StationaryType.RETROGRADE_TO_DIRECT
 import destiny.core.calendar.GmtJulDay
+import destiny.core.calendar.toInstant
 import java.io.Serializable
+import kotlin.time.Duration
+
+enum class RetrogradePhase {
+  PREPARING,
+  RETROGRADING,
+  LEAVING
+}
+
+data class RetrogradeSpan(val fromGmt: GmtJulDay, val toGmtJulDay: GmtJulDay, val fromPos: IPos, val toPos: IPos) {
+  val duration: Duration by lazy {
+    toGmtJulDay.toInstant().minus(fromGmt.toInstant())
+  }
+}
+
+data class StarRetrogradePhases(val star: Star,
+                                val preparingGmt: GmtJulDay, val preparingPos: IPos,
+                                val retrogradingGmt: GmtJulDay, val retrogradingPos: IPos,
+                                val returningGmt: GmtJulDay, val returningPos: IPos,
+                                val leavingGmt: GmtJulDay, val leavingPos: IPos) {
+
+  val phaseMap : Map<RetrogradePhase , RetrogradeSpan> by lazy {
+    mapOf(
+      PREPARING to RetrogradeSpan(preparingGmt, retrogradingGmt, preparingPos, retrogradingPos),
+      RETROGRADING to RetrogradeSpan(retrogradingGmt, returningGmt, retrogradingPos, returningPos),
+      LEAVING to RetrogradeSpan(returningGmt, leavingGmt, returningPos, leavingPos)
+    )
+  }
+}
 
 /** 星體順逆資訊 */
-data class StarRetrograde(val gmtJulDay: GmtJulDay, val star: Star, val type: StationaryType, val pos: IStarPos) : Serializable
+data class StarRetrograde(val gmtJulDay: GmtJulDay, val star: Star, val type: StationaryType, val pos: IPos) : Serializable
 
 /**
  * 計算星體在黃道帶上 逆行 / Stationary (停滯) 的介面，目前 SwissEph 的實作只支援 Planet , Asteroid , Moon's Node (只有 True Node。 Mean 不會逆行！)
@@ -21,22 +55,104 @@ interface IRetrograde {
   fun getNextStationary(star: Star, fromGmt: GmtJulDay, forward: Boolean): GmtJulDay
 
   /**
+   * 下次停滯的時間為何時 (GMT) , 黃道座標為何
+   */
+  fun getNextStationaryPos(star: Star, fromGmt: GmtJulDay, forward: Boolean, starPositionImpl: IStarPosition<*>) : Pair<GmtJulDay, IPos> {
+    return getNextStationary(star, fromGmt, forward).let { gmtJulDay ->
+      gmtJulDay to starPositionImpl.getPosition(star, gmtJulDay, GEO, ECLIPTIC)
+    }
+  }
+
+  /**
+   * 取得星體逆行三態
+   */
+  fun getNextStationaryPhases(star: Star, fromGmt: GmtJulDay, forward: Boolean, starPositionImpl: IStarPosition<*>, transit: IStarTransit) : StarRetrogradePhases {
+    val (nextStationary, nextPos) = getNextStationaryPos(star, fromGmt, forward, starPositionImpl)
+
+    val prior = nextStationary - 1 / 1440.0
+    val after = nextStationary + 1 / 1440.0
+    val pos1 = starPositionImpl.getPosition(star, prior, GEO, ECLIPTIC)
+    val pos2 = starPositionImpl.getPosition(star, after, GEO, ECLIPTIC)
+
+    val type: StationaryType = if (pos1.speedLng > 0 && pos2.speedLng < 0)
+      DIRECT_TO_RETROGRADE
+    else if (pos1.speedLng < 0 && pos2.speedLng > 0)
+      RETROGRADE_TO_DIRECT
+    else
+      throw RuntimeException("Error , 滯留前 speed = " + pos1.speedLng + " , 滯留後 speed = " + pos2.speedLng)
+
+
+    val retrogradingGmt: GmtJulDay
+    val retrogradingPos: IPos
+
+    val returningGmt: GmtJulDay
+    val returningPos: IPos
+
+
+    when(type) {
+      DIRECT_TO_RETROGRADE -> {
+
+//        val (prevReturningGmt , prevReturningPos) = getNextStationaryPos(star, nextStationary, false, starPositionImpl)
+//        val (prevRetroGmt, prevRetroPos) = getNextStationaryPos(star, prevReturningGmt, false, starPositionImpl)
+//        val leavingGmt = transit.getNextTransitGmt(star, prevRetroPos.lngDeg, prevReturningGmt, false, ECLIPTIC)
+//        if (leavingGmt > fromGmt){
+//          retrogradingGmt = prevRetroGmt
+//          retrogradingPos = prevRetroPos
+//
+//          returningGmt = prevReturningGmt
+//          returningPos = prevReturningPos
+//        } else {
+//
+//        }
+
+        getNextStationaryPos(star, nextStationary, true, starPositionImpl).also {
+          returningGmt = it.first
+          returningPos = it.second
+        }
+
+        retrogradingGmt = nextStationary
+        retrogradingPos = nextPos
+
+
+      }
+      RETROGRADE_TO_DIRECT -> {
+        getNextStationaryPos(star, fromGmt, false, starPositionImpl).also {
+          retrogradingGmt = it.first
+          retrogradingPos = it.second
+        }
+
+        returningGmt = nextStationary
+        returningPos = nextPos
+      }
+    }
+
+    val preparingGmt = transit.getNextTransitGmt(star, returningPos.lngDeg, retrogradingGmt, false, ECLIPTIC)
+    val preparingPos = starPositionImpl.getPosition(star, preparingGmt, GEO, ECLIPTIC)
+
+    val leavingGmt = transit.getNextTransitGmt(star, retrogradingPos.lngDeg, returningGmt, true, ECLIPTIC)
+    val leavingPos = starPositionImpl.getPosition(star, leavingGmt, GEO, ECLIPTIC)
+
+    return StarRetrogradePhases(star, preparingGmt, preparingPos, retrogradingGmt, retrogradingPos, returningGmt, returningPos, leavingGmt, leavingPos)
+  }
+
+
+  /**
    * 承上，不僅計算下次（或上次）的停滯時間
    * 另外計算，該次停滯，是準備「順轉逆」，或是「逆轉順」
    */
   fun getNextStationary(star: Star, fromGmt: GmtJulDay, forward: Boolean, starPositionImpl: IStarPosition<*>): StarRetrograde {
     val nextStationary: GmtJulDay = getNextStationary(star, fromGmt, forward)
-    val pos = starPositionImpl.getPosition(star, nextStationary, Centric.GEO, Coordinate.ECLIPTIC)
+    val pos = starPositionImpl.getPosition(star, nextStationary, GEO, ECLIPTIC)
     // 分別取 滯留前、後 來比對
     val prior = nextStationary - 1 / 1440.0
     val after = nextStationary + 1 / 1440.0
-    val pos1 = starPositionImpl.getPosition(star, prior, Centric.GEO, Coordinate.ECLIPTIC)
-    val pos2 = starPositionImpl.getPosition(star, after, Centric.GEO, Coordinate.ECLIPTIC)
+    val pos1 = starPositionImpl.getPosition(star, prior, GEO, ECLIPTIC)
+    val pos2 = starPositionImpl.getPosition(star, after, GEO, ECLIPTIC)
 
     val type = if (pos1.speedLng > 0 && pos2.speedLng < 0)
-      StationaryType.DIRECT_TO_RETROGRADE
+      DIRECT_TO_RETROGRADE
     else if (pos1.speedLng < 0 && pos2.speedLng > 0)
-      StationaryType.RETROGRADE_TO_DIRECT
+      RETROGRADE_TO_DIRECT
     else
       throw RuntimeException("Error , 滯留前 speed = " + pos1.speedLng + " , 滯留後 speed = " + pos2.speedLng)
 
