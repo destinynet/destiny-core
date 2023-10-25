32ce18da29cea20ff6ffd1e05fae2b249e6e5795
smallufo <smallufo@gmail.com>
2023-10-25 08:27:36 +0800

    Domain to ai package

---
+++ src/main/kotlin/destiny/tools/ai/Domain.kt
@@ -0,0 +1,28 @@
+package destiny.tools.ai
+
+import destiny.tools.converters.EnumMapConverter
+import destiny.tools.converters.ParserCommons
+
+enum class Domain {
+  /** 紫微斗數 */
+  ZIWEI,
+
+  /** 易經隨機起卦 */
+  ICHING_RAND,
+
+  /** 占星本命盤 */
+  HOROSCOPE,
+
+  /** 塔羅占卜 */
+  TAROT,
+
+  /** 籤詩 */
+  CHANCE,
+}
+
+object DomainConverter : EnumMapConverter<Domain>("domain") {
+  private fun readResolve(): Any = DomainConverter
+  override fun getContext(map: Map<String, String>): Domain? {
+    return ParserCommons.parseEnum(key, Domain::class.java, map)
+  }
+}
