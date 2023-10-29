29f607d57187563eb7fd7be90dabcf18939d0051
smallufo <smallufo@gmail.com>
2023-10-29 22:18:28 +0800

    paypal OrderApprovedEvent, ZonedDateTimeSerializer

---
+++ src/test/kotlin/destiny/tools/serializers/ZonedDateTimeSerializerTest.kt
@@ -0,0 +1,43 @@
+/**
+ * Created by smallufo on 2023-10-29.
+ */
+package destiny.tools.serializers
+
+import kotlinx.serialization.json.Json
+import mu.KotlinLogging
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class ZonedDateTimeSerializerTest {
+
+  private val logger = KotlinLogging.logger { }
+
+  private val json = Json { encodeDefaults = false }
+
+  @Test
+  fun asiaTaipei() {
+    val zdt = ZonedDateTime.of(LocalDateTime.of(2023, 10, 29, 21, 40), ZoneId.of("Asia/Taipei"))
+    json.encodeToString(ZonedDateTimeSerializer, zdt).also { serialized ->
+      logger.info { serialized }
+      assertNotNull(serialized)
+      val deserialized = json.decodeFromString(ZonedDateTimeSerializer, serialized)
+      assertEquals(zdt, deserialized)
+    }
+  }
+
+  @Test
+  fun utc() {
+    val zdt = ZonedDateTime.of(LocalDateTime.of(2023, 10, 29, 21, 40), ZoneId.of("UTC"))
+    json.encodeToString(ZonedDateTimeSerializer, zdt).also { serialized ->
+      logger.info { serialized }
+      assertNotNull(serialized)
+      val deserialized = json.decodeFromString(ZonedDateTimeSerializer, serialized)
+      assertEquals(zdt, deserialized)
+    }
+  }
+
+}
