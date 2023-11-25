9256094ce118d9acb983edbbb57c97a4da1f7575
smallufo <smallufo@gmail.com>
2023-11-25 18:59:42 +0800

    SpreadFiveCross (十字陣) : builder , digester , tests

---
+++ src/main/kotlin/destiny/core/tarot/SpreadFiveCross.kt
@@ -0,0 +1,30 @@
+/**
+ * Created by smallufo on 2023-11-25.
+ */
+package destiny.core.tarot
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.util.*
+
+@Serializable
+@SerialName("Cross5Spread")
+data class SpreadFiveCross(
+  val center: CardOrientation,
+  val left: CardOrientation,
+  val right: CardOrientation,
+  val top: CardOrientation,
+  val bottom: CardOrientation
+) : ISpread {
+
+  override val cards: List<CardOrientation>
+    get() = listOf(center, left, right, top, bottom)
+
+  override fun getTitle(locale: Locale): String {
+    return when(locale.language) {
+      "en" -> "The Cross Spread"
+      "ja" -> "十字のスプレッド"
+      else -> "十字陣"
+    }
+  }
+}
