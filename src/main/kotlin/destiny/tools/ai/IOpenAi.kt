a9c13f0cfdef7a651726412f697ea66943b54b8d
smallufo <smallufo@gmail.com>
2023-12-03 22:15:10 +0800

    remove OpenAI deprecated textComplete code

--- src/main/kotlin/destiny/tools/ai/IOpenAi.kt
+++ src/main/kotlin/destiny/tools/ai/IOpenAi.kt
@@ -7,9 +7,4 @@ interface IOpenAi : IChatCompletion {
 
   @Serializable
   data class OpenAiMsg(val role: String, val content: String)
-
-  //suspend fun chatComplete(messages: List<Msg>, user: String? = null, timeout : Duration = Duration.of(60, ChronoUnit.SECONDS)): Reply
-
-  // TODO : LEGACY 用不到，可以移除，未來 OpenAi 也會移除
-  suspend fun textComplete(prompt: String, user: String? = null): Reply
 }
