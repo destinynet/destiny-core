0a10c06218abeb1abbbd51f1db9c81abb4dc4045
smallufo <smallufo@gmail.com>
2023-12-19 21:21:16 +0800

    SpreadSixCards . Digester TODO

---
+++ src/main/kotlin/destiny/core/tarot/SpreadSixCards.kt
@@ -0,0 +1,22 @@
+package destiny.core.tarot
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import java.util.*
+
+/**
+ * Created by smallufo on 2023-12-19.
+ */
+@Serializable
+@SerialName("6cards")
+data class SpreadSixCards(val lt: CardOrientation, val mt: CardOrientation, val rt: CardOrientation,
+                          val lb: CardOrientation, val mb: CardOrientation, val rb: CardOrientation) : ISpread {
+  override val cards: List<CardOrientation> = listOf(lt, mt, rt, lb, mb, rb)
+  override fun getTitle(locale: Locale): String {
+    return when (locale.language) {
+      "en" -> "Six Card Spread"
+      "ja" -> "六枚カード"
+      else -> "六牌法"
+    }
+  }
+}
