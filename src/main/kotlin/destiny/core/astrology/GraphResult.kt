bac5df4f53fbd254fb9ec0d7a0a68c55e8358f32
smallufo <smallufo@gmail.com>
2024-09-13 18:38:03 +0800

    GraphResult add pathTerminals

--- src/main/kotlin/destiny/core/astrology/GraphResult.kt
+++ src/main/kotlin/destiny/core/astrology/GraphResult.kt
@@ -6,5 +6,12 @@ data class GraphResult(
   val circles: Set<Circular<Planet>>,
   val paths: Set<List<Planet>>,
   val isolated: Set<Planet>,
+  /* isolated path terminals */
   val terminals: Set<Planet>
-)
+) {
+  /** path 的端點，不論是否是 連接上 circle */
+  val pathTerminals : Set<Planet>
+    get() {
+      return paths.map { planets -> planets.last() }.toSet()
+    }
+}
