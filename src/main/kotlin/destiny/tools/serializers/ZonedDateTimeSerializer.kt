5f59874484ee1c9e1d67838062994fd7b8082be6
smallufo <smallufo@gmail.com>
2024-10-07 20:23:35 +0800

    enhance ZonedDateTimeSerializer

--- src/main/kotlin/destiny/tools/serializers/ZonedDateTimeSerializer.kt
+++ src/main/kotlin/destiny/tools/serializers/ZonedDateTimeSerializer.kt
@@ -10,6 +10,7 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 
 object ZonedDateTimeSerializer : KSerializer<ZonedDateTime> {
@@ -17,11 +18,12 @@ object ZonedDateTimeSerializer : KSerializer<ZonedDateTime> {
   override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("ZonedDateTime", PrimitiveKind.STRING)
 
   override fun serialize(encoder: Encoder, value: ZonedDateTime) {
-    encoder.encodeString(value.toString())
+    val formatted = value.format(DateTimeFormatter.ISO_INSTANT)
+    encoder.encodeString(formatted)
   }
 
   override fun deserialize(decoder: Decoder): ZonedDateTime {
     val string = decoder.decodeString()
-    return ZonedDateTime.parse(string)
+    return ZonedDateTime.parse(string, DateTimeFormatter.ISO_INSTANT)
   }
 }
