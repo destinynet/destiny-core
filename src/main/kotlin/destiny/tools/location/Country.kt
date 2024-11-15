c46f32fcdecb73905336ad6158ed5d58a83220c1
smallufo <smallufo@gmail.com>
2024-11-16 01:26:20 +0800

    Country & ICountryFactory / CountryFactory CountryUSA                                            CountryUK                                            CountryGermany                                            CountryFrance                                            CountryItaly                                            CountrySpain , and tests

---
+++ src/main/kotlin/destiny/tools/location/Country.kt
@@ -0,0 +1,13 @@
+/**
+ * Created by smallufo on 2024-11-16.
+ */
+package destiny.tools.location
+
+import destiny.core.calendar.ILatLng
+import destiny.core.calendar.LatValue
+import destiny.core.calendar.LngValue
+
+
+data class Country(val name: String, val code: String, val sections: List<Section>)
+data class Section(val name: String, val code: String, val cities : List<City>)
+data class City(val name: String, override val lat: LatValue, override val lng: LngValue, val unlocode: String) : ILatLng
