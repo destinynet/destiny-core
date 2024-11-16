5f97aead79d49100ee346aa59a3305921d59caee
smallufo <smallufo@gmail.com>
2024-11-17 00:29:47 +0800

    City add regex

--- src/main/kotlin/destiny/tools/location/Country.kt
+++ src/main/kotlin/destiny/tools/location/Country.kt
@@ -10,4 +10,4 @@ import destiny.core.calendar.LngValue
 
 data class Country(val name: String, val code: String, val sections: List<Section>)
 data class Section(val name: String, val code: String, val cities : List<City>)
-data class City(val name: String, override val lat: LatValue, override val lng: LngValue, val unlocode: String) : ILatLng
+data class City(val name: String, override val lat: LatValue, override val lng: LngValue, val unlocode: String, val regex: Regex? = null) : ILatLng
