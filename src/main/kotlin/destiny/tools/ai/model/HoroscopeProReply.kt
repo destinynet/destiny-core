f2f5bdc8d683c6872f1c5a5d39b60964da475b48
smallufo <smallufo@gmail.com>
2024-12-26 01:58:47 +0800

    HoroscopeProReply

---
+++ src/main/kotlin/destiny/tools/ai/model/HoroscopeProReply.kt
@@ -0,0 +1,29 @@
+/**
+ * Created by smallufo on 2024-12-26.
+ */
+package destiny.tools.ai.model
+
+import destiny.core.Advice
+import destiny.core.astrology.AstroPattern
+import destiny.core.astrology.IMidPointWithFocal
+import destiny.core.astrology.IPointAspectPattern
+
+
+class HoroscopeProReply {
+
+  data class AspectAnalysis(
+    val pattern: IPointAspectPattern,
+    val advice: Advice
+  ) : IDigestResponse
+
+  data class PatternAnalysis(
+    val pattern: AstroPattern,
+    val advice: Advice
+  ) : IDigestResponse
+
+  data class MidpointAnalysis(
+    val midpoint: IMidPointWithFocal,
+    val advice: Advice
+  ) : IDigestResponse
+
+}
