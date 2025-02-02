0b2f79b1af9e4f53625b7d283b0225d165e265ec
smallufo <smallufo@gmail.com>
2025-02-02 22:24:47 +0800

    IBirthDataSerializer to make use of GenderSerializer and preserve legacy mode.

--- src/main/kotlin/destiny/tools/serializers/IBirthDataSerializer.kt
+++ src/main/kotlin/destiny/tools/serializers/IBirthDataSerializer.kt
@@ -9,10 +9,7 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.descriptors.buildClassSerialDescriptor
 import kotlinx.serialization.descriptors.element
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.encoding.decodeStructure
-import kotlinx.serialization.encoding.encodeStructure
+import kotlinx.serialization.encoding.*
 import java.time.LocalDateTime
 import java.util.*
 
@@ -36,11 +33,18 @@ object IBirthDataSerializer : KSerializer<IBirthData> {
     var gender = Gender.男
 
     decoder.decodeStructure(descriptor) {
-      loop@ while (true) {
-        when (decodeElementIndex(descriptor)) {
-          0    -> timeLoc = decodeSerializableElement(descriptor, 0, ITimeLocSerializer)
-          1    -> gender = decodeSerializableElement(descriptor, 1, GenderSerializer)
-          else -> break@loop
+      while (true) {
+        when (val index = decodeElementIndex(descriptor)) {
+          0                            -> timeLoc = decodeSerializableElement(descriptor, 0, ITimeLocSerializer)
+          1                            -> gender = try {
+            decodeSerializableElement(descriptor, 1, GenderSerializer)
+          } catch (e: Exception) {
+            // TODO : legacy style , to be removed in the future (maybe at end of 2025)
+            if (decodeBooleanElement(descriptor, 1)) Gender.男 else Gender.女
+          }
+
+          CompositeDecoder.DECODE_DONE -> break
+          else                         -> error("Unexpected index: $index")
         }
       }
     }
