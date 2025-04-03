e517b01bb5e0001d18eed37f6f16b1ba33b11f0d
smallufo <smallufo@gmail.com>
2025-04-03 16:29:26 +0800

    SynastryAspect , SynastryDomain , properties and prompts

---
+++ src/test/kotlin/destiny/core/SynastryAspectTest.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by smallufo on 2025-04-03.
+ */
+package destiny.core
+
+import kotlin.test.Test
+
+class SynastryAspectTest : EnumTest() {
+
+  @Test
+  fun testString() {
+    testEnums(SynastryAspect::class)
+  }
+}
---
+++ src/test/kotlin/destiny/core/SynastryDomainTest.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by smallufo on 2025-04-03.
+ */
+package destiny.core
+
+import kotlin.test.Test
+
+class SynastryDomainTest : EnumTest() {
+
+  @Test
+  fun testString() {
+    testEnums(SynastryDomain::class)
+  }
+}
