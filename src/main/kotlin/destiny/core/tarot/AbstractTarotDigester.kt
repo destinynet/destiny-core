0ccbbca8acdec4183cc0baf18de620b4ec4fb220
smallufo <smallufo@gmail.com>
2025-05-05 23:46:54 +0800

    LocalDate[Ew|Astro]Digest

--- src/main/kotlin/destiny/core/tarot/AbstractTarotDigester.kt
+++ src/main/kotlin/destiny/core/tarot/AbstractTarotDigester.kt
@@ -6,14 +6,9 @@ package destiny.core.tarot
 import destiny.tools.ai.model.AbstractDigestFormat
 import destiny.tools.ai.model.Domain
 import destiny.tools.ai.model.FormatSpec
-import java.util.*
 
 
 abstract class AbstractTarotDigester<T : ISpread> : AbstractDigestFormat<T, String>(FormatSpec.of<String>("tarot" , "tarot content")) {
 
   final override val domain: Domain = Domain.TAROT
-
-  override fun digestWithoutFormat(model: T, locale: Locale): String? {
-    return null
-  }
 }
