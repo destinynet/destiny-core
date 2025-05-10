ebabf2347564d2500934115464caff9acaff5fe5
smallufo <smallufo@gmail.com>
2025-05-11 02:10:42 +0800

    Groq MODEL_QWEN_QWQ_32B = "qwen-qwq-32b" and tests

--- src/main/kotlin/destiny/tools/ai/Groq.kt
+++ src/main/kotlin/destiny/tools/ai/Groq.kt
@@ -3,20 +3,37 @@
  */
 package destiny.tools.ai
 
+import destiny.tools.ai.OpenAi.FunctionDeclaration
+import destiny.tools.ai.OpenAi.Message.ToolCall
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 
 class Groq {
 
   @Serializable
-  data class Message(val role: String, val content: String)
+  data class Message(val role: String,
+                     val content: String?,
+                     @Transient
+                     val reasoning: String? = null,
+                     @SerialName("tool_call_id") val toolCallId: String? = null,
+                     @SerialName("tool_calls") val toolCalls: List<ToolCall>? = null
+  )
 
   @Serializable
-  data class Response(val id: String, val model: String, val choices: List<Choice>) {
+  data class Response(val id: String, val model: String, val choices: List<Choice>, val usage: Usage) {
     @Serializable
     data class Choice(
       val index: Int, val message: Message, @SerialName("finish_reason") val finishReason: String
+    )
+
+    @Serializable
+    data class Usage(
+      @SerialName("prompt_tokens")
+      val promptTokens: Int,
+      @SerialName("completion_tokens")
+      val completionTokens: Int,
     )
   }
 
@@ -32,7 +49,13 @@ class Groq {
   }
 
   @Serializable
-  data class ChatModel(val model: String, val messages: List<Message>, val user: String?, val temperature: Double = 1.0,
+  data class ChatModel(val model: String,
+                       val messages: List<Message>,
+                       val user: String?,
+                       val temperature: Double = 1.0,
+                       val tools: List<FunctionDeclaration>? = null,
+                       @SerialName("max_completion_tokens")
+                       val maxCompletionTokens: Int? = null,
                        @kotlinx.serialization.Transient
                        val jsonSchemaSpec: JsonSchemaSpec? = null) {
 
