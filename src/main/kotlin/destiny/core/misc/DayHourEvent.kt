0f5f5d061b70d5beecdddead5da19abe90faa1e4
smallufo <smallufo@gmail.com>
2025-05-28 03:21:31 +0800

    DayHourSelectionService , DaySelectionOutputService init work and tests

---
+++ src/main/kotlin/destiny/core/misc/DayHourEvent.kt
@@ -0,0 +1,120 @@
+package destiny.core.misc
+
+import destiny.core.FlowScale
+import destiny.core.Scale
+import destiny.core.astrology.AspectData
+import destiny.core.calendar.GmtJulDay
+import destiny.core.calendar.eightwords.FlowPattern
+import destiny.core.calendar.eightwords.IEightWords
+import destiny.core.calendar.eightwords.Reacting
+
+/**
+ * 擇日、則時 Event
+ */
+sealed class DayHourEvent {
+
+  abstract val gmtJulDay: GmtJulDay
+
+  enum class Type {
+    GOOD,
+    BAD
+  }
+
+  abstract val type: Type
+
+  enum class Span {
+    DAY,
+    HOUR, // 八字時辰 , unit = 2Hour
+    INSTANT
+  }
+
+  abstract val span: Span
+
+  sealed class EwEvent(val flowPattern: FlowPattern) : DayHourEvent() {
+    abstract val outerScale: Scale
+    abstract val outer: IEightWords
+    override val span: Span
+      get() = when (outerScale) {
+        Scale.DAY  -> Span.DAY
+        Scale.HOUR -> Span.HOUR
+        else       -> throw IllegalArgumentException("impossible")
+      }
+
+    /** 本命某天干, 同時受到 流日、流時 的影響 */
+    data class StemAffecting(override val gmtJulDay: GmtJulDay, val pattern: FlowPattern.Affecting, override val outer: IEightWords) : EwEvent(pattern) {
+      override val outerScale: Scale = Scale.HOUR
+      override val type: Type
+        get() = when (pattern.reacting) {
+          Reacting.SAME, Reacting.PRODUCED                         -> Type.GOOD
+          Reacting.BEATEN, Reacting.PRODUCING, Reacting.DOMINATING -> Type.BAD
+        }
+    }
+
+    /** 天干五合 , 限定只比對流日、流時的天干 , 對本命哪個 天干 五合 */
+    data class StemCombined(override val gmtJulDay: GmtJulDay, val pattern: FlowPattern.StemCombined, override val outer: IEightWords) : EwEvent(pattern) {
+      override val type: Type = Type.GOOD
+      override val outerScale: Scale
+        get() = when (pattern.flowScale) {
+          FlowScale.DAY  -> Scale.DAY
+          FlowScale.HOUR -> Scale.HOUR
+          else           -> throw IllegalArgumentException("impossible")
+        }
+    }
+
+    /** 地支六合 , 限定只比對 流日、流時的地支 , 對本命哪個 地支 六合 */
+    data class BranchCombined(override val gmtJulDay: GmtJulDay, val pattern: FlowPattern.BranchCombined, override val outer: IEightWords) : EwEvent(pattern) {
+      override val type: Type = Type.GOOD
+      override val outerScale: Scale
+        get() = when (pattern.flowScale) {
+          FlowScale.DAY  -> Scale.DAY
+          FlowScale.HOUR -> Scale.HOUR
+          else           -> throw IllegalArgumentException("impossible")
+        }
+    }
+
+    /** 地支三合 , 限定比對 哪個流日 or 流時 , 對本命兩個地支造成三合 */
+    data class TrilogyToFlow(override val gmtJulDay: GmtJulDay, val pattern: FlowPattern.TrilogyToFlow, override val outer: IEightWords) : EwEvent(pattern) {
+      override val type: Type = Type.GOOD
+      override val outerScale: Scale
+        get() = when (pattern.flow.first) {
+          FlowScale.DAY  -> Scale.DAY
+          FlowScale.HOUR -> Scale.HOUR
+          else           -> throw IllegalArgumentException("impossible")
+        }
+
+    }
+
+    /** 本命某地支 , 與流運 某兩柱 (其中一個必須是 DAY or HOUR) 形成三合 , 其中，限制 [FlowPattern.ToFlowTrilogy.flows] 必須至少包含 [FlowScale.DAY] or [FlowScale.HOUR] */
+    data class ToFlowTrilogy(override val gmtJulDay: GmtJulDay, val pattern: FlowPattern.ToFlowTrilogy, override val outer: IEightWords) : EwEvent(pattern) {
+      override val type: Type = Type.GOOD
+      override val outerScale: Scale
+        get() {
+          val flowScales: List<FlowScale> = pattern.flows.map { it.first }
+          return if (flowScales.contains(FlowScale.HOUR)) {
+            Scale.HOUR
+          } else if (flowScales.contains(FlowScale.DAY)) {
+            Scale.DAY
+          } else {
+            throw IllegalArgumentException("impossible")
+          }
+        }
+    }
+
+    /** 地支正沖 , 哪個流日 or 流時 , 正沖本命哪 地支 */
+    data class BranchOpposition(override val gmtJulDay: GmtJulDay, val pattern: FlowPattern.BranchOpposition, override val outer: IEightWords) : EwEvent(pattern) {
+      override val type: Type = Type.BAD
+      override val outerScale: Scale
+        get() = when (pattern.flowScale) {
+          FlowScale.DAY  -> Scale.DAY
+          FlowScale.HOUR -> Scale.HOUR
+          else           -> throw IllegalArgumentException("impossible")
+        }
+    }
+  }
+
+  /** 占星事件 */
+  data class AstroEvent(override val type: Type, val aspectData: AspectData) : DayHourEvent() {
+    override val span: Span = Span.INSTANT
+    override val gmtJulDay: GmtJulDay = aspectData.gmtJulDay
+  }
+}
---
+++ src/main/kotlin/destiny/core/misc/DayHourSelectionService.kt
@@ -0,0 +1,143 @@
+/**
+ * Created by smallufo on 2025-05-28.
+ */
+package destiny.core.misc
+
+import destiny.core.FlowScale
+import destiny.core.IBirthDataNamePlace
+import destiny.core.astrology.*
+import destiny.core.astrology.Planet.*
+import destiny.core.astrology.ZodiacDegree.Companion.toZodiacDegree
+import destiny.core.calendar.GmtJulDay
+import destiny.core.calendar.ILocation
+import destiny.core.calendar.TimeTools.toGmtJulDay
+import destiny.core.calendar.eightwords.*
+import destiny.core.calendar.eightwords.FlowDayHourPatterns.affecting
+import destiny.core.calendar.eightwords.FlowDayHourPatterns.branchCombined
+import destiny.core.calendar.eightwords.FlowDayHourPatterns.branchOpposition
+import destiny.core.calendar.eightwords.FlowDayHourPatterns.stemCombined
+import destiny.core.calendar.eightwords.FlowDayHourPatterns.toFlowTrilogy
+import destiny.core.calendar.eightwords.FlowDayHourPatterns.trilogyToFlow
+import destiny.core.chinese.eightwords.PersonPresentConfig
+import destiny.core.chinese.eightwords.PersonPresentFeature
+import jakarta.inject.Named
+import java.time.LocalDate
+
+
+@Named
+class DayHourSelectionService(
+  private val ewPersonPresentFeature: PersonPresentFeature,
+  private val ewFeature: EightWordsFeature,
+  private val starPositionImpl: IStarPosition<*>,
+  private val starTransitImpl: IStarTransit
+) {
+
+  fun search(bdnp: IBirthDataNamePlace, fromDate: LocalDate, toDate: LocalDate, loc: ILocation = bdnp.location, ewConfig: IPersonPresentConfig = PersonPresentConfig()): Sequence<DayHourEvent> {
+
+    require(toDate.isAfter(fromDate)) { "toDate must be after the fromDate" }
+
+    val fromGmtJulDay = fromDate.atTime(0, 0).toGmtJulDay(loc)
+    val toGmtJulDay = toDate.plusDays(1).atTime(0, 0).toGmtJulDay(loc)
+
+    return  (searchAstrologyEvents(bdnp, fromGmtJulDay, toGmtJulDay) + searchEw(bdnp, fromGmtJulDay, toGmtJulDay, loc, ewConfig))
+  }
+
+  private fun searchAstrologyEvents(bdnp: IBirthDataNamePlace, fromGmtJulDay: GmtJulDay, toGmtJulDay: GmtJulDay): Sequence<DayHourEvent.AstroEvent> {
+    val innerStars = setOf(SUN, MOON, MERCURY, VENUS, MARS, JUPITER, SATURN)
+
+    val harmonyAngles = setOf(0.0, 60.0, 120.0, 240.0, 300.0)
+    val tensionAngles = setOf(90.0, 180.0)
+
+    val innerStarPosMap: Map<Planet, ZodiacDegree> = innerStars.associateWith { planet ->
+      starPositionImpl.getPosition(planet, bdnp.gmtJulDay, bdnp.location).lngDeg
+    }
+
+    fun searchEvents(outerStars: Set<Planet>, angles: Set<Double>): Sequence<AspectData> {
+      return outerStars.asSequence().flatMap { outer ->
+        innerStars.flatMap { inner ->
+          val innerDeg: ZodiacDegree = innerStarPosMap[inner]!!
+          val degrees = angles.map { it.toZodiacDegree() }.map { it + innerDeg }.toSet()
+          starTransitImpl.getRangeTransitGmt(outer, degrees, fromGmtJulDay, toGmtJulDay, true, Coordinate.ECLIPTIC).map { (zDeg, gmt) ->
+            val angle: Double = zDeg.getAngle(innerDeg)
+            val pattern = PointAspectPattern(listOf(outer, inner), angle, null, 0.0)
+            AspectData(pattern, null, 0.0, null, gmt)
+          }
+        }
+      }
+    }
+
+    return sequenceOf(
+      searchEvents(innerStars, harmonyAngles).map { aspectData -> DayHourEvent.AstroEvent(DayHourEvent.Type.GOOD, aspectData) },
+      searchEvents(innerStars, tensionAngles).map { aspectData -> DayHourEvent.AstroEvent(DayHourEvent.Type.BAD, aspectData) }
+    ).flatten()
+  }
+
+  private fun matchEwEvents(gmtJulDay: GmtJulDay, outer: IEightWords, inner: IEightWords): Sequence<DayHourEvent.EwEvent> {
+    val affecting = with(affecting) {
+      inner.getPatterns(outer.day, outer.hour).asSequence().map { pattern ->
+        pattern as FlowPattern.Affecting
+      }.map { pattern -> DayHourEvent.EwEvent.StemAffecting(gmtJulDay, pattern, outer) }
+    }
+
+    val stemCombined = with(stemCombined) {
+      inner.getPatterns(outer.day, outer.hour).asSequence().map { pattern: FlowPattern ->
+        pattern as FlowPattern.StemCombined
+      }.map { pattern -> DayHourEvent.EwEvent.StemCombined(gmtJulDay, pattern, outer) }
+    }
+
+    val branchCombined = with(branchCombined) {
+      inner.getPatterns(outer.day, outer.hour).asSequence().map { pattern: FlowPattern ->
+        pattern as FlowPattern.BranchCombined
+      }.map { pattern -> DayHourEvent.EwEvent.BranchCombined(gmtJulDay, pattern, outer) }
+    }
+
+    val trilogyToFlow = with(trilogyToFlow) {
+      inner.getPatterns(outer.day, outer.hour).asSequence().map { pattern: FlowPattern ->
+        pattern as FlowPattern.TrilogyToFlow
+      }.map { pattern -> DayHourEvent.EwEvent.TrilogyToFlow(gmtJulDay, pattern, outer) }
+    }
+
+    val toFlowTrilogy = with(toFlowTrilogy) {
+      inner.getPatterns(outer.day, outer.hour).asSequence().map { pattern: FlowPattern ->
+        pattern as FlowPattern.ToFlowTrilogy
+      }.filter { pattern ->
+        /**
+         * 限制 [FlowPattern.ToFlowTrilogy.flows] 必須至少包含 [FlowScale.DAY] or [FlowScale.HOUR]
+         * 這樣找「流日」「流時」才有意義
+         */
+        val flowScales = pattern.flows.map { it.first }
+        flowScales.any { it == FlowScale.DAY || it == FlowScale.HOUR }
+      }.map { pattern -> DayHourEvent.EwEvent.ToFlowTrilogy(gmtJulDay, pattern, outer) }
+    }
+
+    val branchOpposition = with(branchOpposition) {
+      inner.getPatterns(outer.day, outer.hour).asSequence().map { pattern: FlowPattern ->
+        pattern as FlowPattern.BranchOpposition
+      }.map { pattern -> DayHourEvent.EwEvent.BranchOpposition(gmtJulDay, pattern, outer) }
+    }
+
+    return sequenceOf(affecting, stemCombined, branchCombined, trilogyToFlow, toFlowTrilogy, branchOpposition).flatten()
+  }
+
+  private fun searchEw(
+    bdnp: IBirthDataNamePlace,
+    fromGmtJulDay: GmtJulDay,
+    toGmtJulDay: GmtJulDay,
+    loc: ILocation = bdnp.location,
+    config: IPersonPresentConfig = PersonPresentConfig()
+  ): Sequence<DayHourEvent.EwEvent> {
+
+    val personEw = ewPersonPresentFeature.getPersonModel(bdnp, config).eightWords
+    logger.info { "本命八字 : $personEw" }
+    val fromEw: IEightWords = ewFeature.getModel(fromGmtJulDay, loc, config)
+
+    return generateSequence(fromEw to fromGmtJulDay) { (outerEw, gmtJulDay) ->
+
+
+      ewFeature.next(gmtJulDay + 0.01, loc, config)
+    }.takeWhile { (outerEw, gmtJulDay) -> gmtJulDay < toGmtJulDay }
+      .flatMap { (outerEw, gmtJulDay) ->
+        matchEwEvents(gmtJulDay, outerEw, personEw)
+      }
+  }
+}
