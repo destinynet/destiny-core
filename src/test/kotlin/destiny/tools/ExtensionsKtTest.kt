0da347e241dcffa3fe20b6c9f15f39a0c5f341ca
smallufo <smallufo@gmail.com>
2025-05-29 15:24:03 +0800

    remove unused tzid test

--- src/test/kotlin/destiny/tools/ExtensionsKtTest.kt
+++ src/test/kotlin/destiny/tools/ExtensionsKtTest.kt
@@ -3,16 +3,12 @@
  */
 package destiny.tools
 
-import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.Nested
 import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class ExtensionsKtTest {
-
-  private val logger = KotlinLogging.logger {}
 
   @Test
   fun firstNotNullResult_iterable() {
@@ -38,21 +34,5 @@ class ExtensionsKtTest {
       assertNull(seq.firstNotNullResult { it })
       assertNull(seq.map { it }.firstNotNullResult { it?.lowercase(Locale.getDefault()) })
     }
-  }
-
-  @Nested
-  inner class TzidTest {
-
-    @Test
-    fun nestedObj() {
-      val raw = """
-        {"birthYear": 1988, "birthMonth": 4, "birthDay": 23, "birthHour": 13, "birthMinute": 55, "birthLat": 24.1418685, "birthLng": 120.6755461, 
-        "tzid": {"timeZoneId": "Asia/Taipei"}, 
-        "gender": "M", "forecastYear": 2025, "question": "2025年的運勢如何？"}
-      """.trimIndent()
-
-      assertEquals("Asia/Taipei", Json.parseToJsonElement(raw).toMap()["tzid"])
-    }
-
   }
 }
