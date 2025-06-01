747cbeb9c4f604cdd4b8f966dc8e6b6fbe2b6910
smallufo <smallufo@gmail.com>
2025-06-01 21:59:52 +0800

    DayHourEvent to implement IEvent

--- src/main/kotlin/destiny/core/electional/DayHourEvent.kt
+++ src/main/kotlin/destiny/core/electional/DayHourEvent.kt
@@ -4,14 +4,13 @@ import destiny.core.FlowScale
 import destiny.core.Scale
 import destiny.core.astrology.AspectData
 import destiny.core.calendar.GmtJulDay
+import destiny.core.calendar.IEvent
 import destiny.core.calendar.eightwords.*
 
 /**
- * 擇日、則時 Event
+ * 擇日、擇時 Event
  */
-sealed class DayHourEvent {
-
-  abstract val gmtJulDay: GmtJulDay
+sealed class DayHourEvent : IEvent {
 
   enum class Type {
     GOOD,
@@ -55,7 +54,7 @@ sealed class DayHourEvent {
         }
 
       /** 本命某天干, 同時受到 流日、流時 的影響 */
-      data class StemAffecting(override val gmtJulDay: GmtJulDay, override val pattern: FlowPattern.Affecting, override val outer: IEightWords) : EwPersonalEvent() {
+      data class StemAffecting(override val begin: GmtJulDay, override val pattern: FlowPattern.Affecting, override val outer: IEightWords) : EwPersonalEvent() {
         override val outerScale: Scale = Scale.HOUR
         override val hourRelated: Boolean = pattern.scale == Scale.HOUR
         override val type: Type
@@ -66,7 +65,7 @@ sealed class DayHourEvent {
       }
 
       /** 天干五合 , 限定只比對流日、流時的天干 , 對本命哪個 天干 五合 */
-      data class StemCombined(override val gmtJulDay: GmtJulDay, override val pattern: FlowPattern.StemCombined, override val outer: IEightWords) : EwPersonalEvent() {
+      data class StemCombined(override val begin: GmtJulDay, override val pattern: FlowPattern.StemCombined, override val outer: IEightWords) : EwPersonalEvent() {
         override val type: Type = Type.GOOD
         override val hourRelated: Boolean = pattern.scale == Scale.HOUR
         override val outerScale: Scale
@@ -78,7 +77,7 @@ sealed class DayHourEvent {
       }
 
       /** 地支六合 , 限定只比對 流日、流時的地支 , 對本命哪個 地支 六合 */
-      data class BranchCombined(override val gmtJulDay: GmtJulDay, override val pattern: FlowPattern.BranchCombined, override val outer: IEightWords) : EwPersonalEvent() {
+      data class BranchCombined(override val begin: GmtJulDay, override val pattern: FlowPattern.BranchCombined, override val outer: IEightWords) : EwPersonalEvent() {
         override val type: Type = Type.GOOD
         override val hourRelated: Boolean = pattern.scale == Scale.HOUR
         override val outerScale: Scale
@@ -90,7 +89,7 @@ sealed class DayHourEvent {
       }
 
       /** 地支三合 , 限定比對 哪個流日 or 流時 , 對本命兩個地支造成三合 */
-      data class TrilogyToFlow(override val gmtJulDay: GmtJulDay, override val pattern: FlowPattern.TrilogyToFlow, override val outer: IEightWords) : EwPersonalEvent() {
+      data class TrilogyToFlow(override val begin: GmtJulDay, override val pattern: FlowPattern.TrilogyToFlow, override val outer: IEightWords) : EwPersonalEvent() {
         override val type: Type = Type.GOOD
         override val hourRelated: Boolean = pattern.pairs.map { pair -> pair.first }.any { scale -> scale == Scale.HOUR }
         override val outerScale: Scale
@@ -103,7 +102,7 @@ sealed class DayHourEvent {
       }
 
       /** 本命某地支 , 與流運 某兩柱 (其中一個必須是 DAY or HOUR) 形成三合 , 其中，限制 [FlowPattern.ToFlowTrilogy.flows] 必須至少包含 [FlowScale.DAY] or [FlowScale.HOUR] */
-      data class ToFlowTrilogy(override val gmtJulDay: GmtJulDay, override val pattern: FlowPattern.ToFlowTrilogy, override val outer: IEightWords) : EwPersonalEvent() {
+      data class ToFlowTrilogy(override val begin: GmtJulDay, override val pattern: FlowPattern.ToFlowTrilogy, override val outer: IEightWords) : EwPersonalEvent() {
         override val type: Type = Type.GOOD
         override val hourRelated: Boolean = pattern.scale == Scale.HOUR
         override val outerScale: Scale
@@ -120,7 +119,7 @@ sealed class DayHourEvent {
       }
 
       /** 地支正沖 , 哪個流日 or 流時 , 正沖本命哪 地支 */
-      data class BranchOpposition(override val gmtJulDay: GmtJulDay, override val pattern: FlowPattern.BranchOpposition, override val outer: IEightWords) : EwPersonalEvent() {
+      data class BranchOpposition(override val begin: GmtJulDay, override val pattern: FlowPattern.BranchOpposition, override val outer: IEightWords) : EwPersonalEvent() {
         override val type: Type = Type.BAD
         override val hourRelated: Boolean = pattern.scale == Scale.HOUR
         override val outerScale: Scale
@@ -139,7 +138,7 @@ sealed class DayHourEvent {
     sealed class EwGlobalEvent : EwEvent() {
       override val impact: Impact = Impact.GLOBAL
 
-      data class StemCombined(override val gmtJulDay: GmtJulDay, override val pattern: IdentityPattern.StemCombined, override val outer: IEightWords) : EwGlobalEvent() {
+      data class StemCombined(override val begin: GmtJulDay, override val pattern: IdentityPattern.StemCombined, override val outer: IEightWords) : EwGlobalEvent() {
         override val type: Type = Type.GOOD
         override val span: Span = pattern.pillars.map { it.first }.max().let {
           when (it) {
@@ -150,7 +149,7 @@ sealed class DayHourEvent {
         }
       }
 
-      data class BranchCombined(override val gmtJulDay: GmtJulDay, override val pattern: IdentityPattern.BranchCombined, override val outer: IEightWords) : EwGlobalEvent() {
+      data class BranchCombined(override val begin: GmtJulDay, override val pattern: IdentityPattern.BranchCombined, override val outer: IEightWords) : EwGlobalEvent() {
         override val type: Type = Type.GOOD
         override val span: Span = pattern.pillars.map { it.first }.max().let {
           when (it) {
@@ -161,7 +160,7 @@ sealed class DayHourEvent {
         }
       }
 
-      data class Trilogy(override val gmtJulDay: GmtJulDay, override val pattern: IdentityPattern.Trilogy, override val outer: IEightWords) : EwGlobalEvent() {
+      data class Trilogy(override val begin: GmtJulDay, override val pattern: IdentityPattern.Trilogy, override val outer: IEightWords) : EwGlobalEvent() {
         override val type: Type = Type.GOOD
         override val span: Span = pattern.pillars.map { it.first }.max().let {
           when (it) {
@@ -172,7 +171,7 @@ sealed class DayHourEvent {
         }
       }
 
-      data class BranchOpposition(override val gmtJulDay: GmtJulDay, override val pattern: IdentityPattern.BranchOpposition, override val outer: IEightWords) : EwGlobalEvent() {
+      data class BranchOpposition(override val begin: GmtJulDay, override val pattern: IdentityPattern.BranchOpposition, override val outer: IEightWords) : EwGlobalEvent() {
         override val type: Type = Type.BAD
         override val span: Span = pattern.pillars.map { it.first }.max().let {
           when (it) {
@@ -183,7 +182,7 @@ sealed class DayHourEvent {
         }
       }
 
-      data class StemRooted(override val gmtJulDay: GmtJulDay, override val pattern: IdentityPattern.StemRooted, override val outer: IEightWords) : EwGlobalEvent() {
+      data class StemRooted(override val begin: GmtJulDay, override val pattern: IdentityPattern.StemRooted, override val outer: IEightWords) : EwGlobalEvent() {
         override val type: Type = Type.GOOD
         override val span: Span
           get() = when (
@@ -207,6 +206,6 @@ sealed class DayHourEvent {
   /** 占星事件 */
   data class AstroEvent(override val type: Type, val aspectData: AspectData, override val impact: Impact) : DayHourEvent() {
     override val span: Span = Span.INSTANT
-    override val gmtJulDay: GmtJulDay = aspectData.gmtJulDay
+    override val begin: GmtJulDay = aspectData.gmtJulDay
   }
 }
