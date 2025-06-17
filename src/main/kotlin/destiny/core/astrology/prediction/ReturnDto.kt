91b4e12f3312b882ea044ac99a3ad7db1ac46cd1
smallufo <smallufo@gmail.com>
2025-06-17 12:36:49 +0800

    ReturnDto

--- src/main/kotlin/destiny/core/astrology/prediction/ReturnDto.kt
+++ src/main/kotlin/destiny/core/astrology/prediction/ReturnDto.kt
@@ -6,29 +6,39 @@ package destiny.core.astrology.prediction
 import destiny.core.astrology.IHoroscopeDto
 import destiny.core.astrology.Synastry
 import destiny.tools.serializers.LocalDateTimeSerializer
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import java.time.LocalDateTime
 
 
+interface ReturnDto {
+  val horoscope: IHoroscopeDto
+  val synastry: Synastry
+  val from: LocalDateTime
+  val to: LocalDateTime
+}
+
 @Serializable
 data class SolarReturnDto(
   val year: Int,
-  val solarReturn: IHoroscopeDto,
-  val synastry: Synastry,
+  @SerialName("solarReturn")
+  override val horoscope: IHoroscopeDto,
+  override val synastry: Synastry,
   @Serializable(with = LocalDateTimeSerializer::class)
-  val from: LocalDateTime,
+  override val from: LocalDateTime,
   @Serializable(with = LocalDateTimeSerializer::class)
-  val to: LocalDateTime
-)
+  override val to: LocalDateTime
+) : ReturnDto
 
 @Serializable
 data class LunarReturnDto(
   val year: Int,
   val month: Int,
-  val lunarReturn: IHoroscopeDto,
-  val synastry: Synastry,
+  @SerialName("lunarReturn")
+  override val horoscope: IHoroscopeDto,
+  override val synastry: Synastry,
   @Serializable(with = LocalDateTimeSerializer::class)
-  val from: LocalDateTime,
+  override val from: LocalDateTime,
   @Serializable(with = LocalDateTimeSerializer::class)
-  val to: LocalDateTime
-)
+  override val to: LocalDateTime
+) : ReturnDto
