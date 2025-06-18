333af99198c0eeb4bbcd676870f3a4a20749cdf0
smallufo <smallufo@gmail.com>
2025-06-19 01:35:38 +0800

    fix SynastryDomain and SynastryRelationship

---
+++ src/test/kotlin/destiny/core/SynastryRelationshipTest.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by smallufo on 2025-06-19.
+ */
+package destiny.core
+
+import kotlin.test.Test
+
+class SynastryRelationshipTest : EnumTest() {
+
+  @Test
+  fun testString() {
+    testEnums(SynastryRelationship::class)
+  }
+}
