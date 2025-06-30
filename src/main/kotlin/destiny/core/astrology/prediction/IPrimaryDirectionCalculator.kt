67b9f1fd16587b2a665c172ef9d1de5fd5883083
smallufo <smallufo@gmail.com>
2025-06-30 23:55:12 +0800

    PrimaryDirectionCalculatorZodiacImpl .

--- src/main/kotlin/destiny/core/astrology/prediction/IPrimaryDirectionCalculator.kt
+++ src/main/kotlin/destiny/core/astrology/prediction/IPrimaryDirectionCalculator.kt
@@ -3,6 +3,7 @@
  */
 package destiny.core.astrology.prediction
 
+import destiny.core.astrology.Aspect
 import destiny.core.astrology.AstroPoint
 import destiny.core.astrology.Constants.TROPICAL_YEAR_DAYS
 import destiny.core.astrology.HouseSystem
@@ -14,12 +15,14 @@ import kotlinx.serialization.Serializable
 /** 時間鑰匙的介面，定義了弧角如何轉換為時間 */
 interface ITimeKey {
   fun getArc(years: Double): Double
+  fun getYears(arc: Double): Double
 }
 
 /** 托勒密之鑰：1度赤經 = 1年生命 */
 @Serializable
 object PtolemyKey : ITimeKey {
   override fun getArc(years: Double): Double = years
+  override fun getYears(arc: Double): Double = arc
 }
 
 /** Naibod之鑰：1年 = 0.9856度 (太陽每日平均移動速度) */
@@ -27,6 +30,11 @@ object PtolemyKey : ITimeKey {
 object NaibodKey : ITimeKey {
   private const val NAIBOD_ARC_PER_YEAR = 360.0 /  TROPICAL_YEAR_DAYS // 0.98564736
   override fun getArc(years: Double): Double = years * NAIBOD_ARC_PER_YEAR
+  override fun getYears(arc: Double): Double = arc / NAIBOD_ARC_PER_YEAR
+}
+
+enum class Direction {
+  DIRECT , CONVERSE
 }
 
 /** 代表一個主限法事件 */
@@ -35,7 +43,9 @@ data class DirectionEvent(
   val promissor: AstroPoint,
   val arc: Double, // 推進的弧角
   val years: Double, // 對應的年數
-  val eventGmt: GmtJulDay // 事件發生的約略時間
+  val eventGmt: GmtJulDay, // 事件發生的約略時間
+  val aspect: Aspect,
+  val direction: Direction
 )
 
 interface IPrimaryDirectionCalculator {
