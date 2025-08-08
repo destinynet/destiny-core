837a47dc69422285fa6636169172e0839d7f6648
smallufo <smallufo@gmail.com>
2025-08-08 17:29:10 +0800

    enhance Decoder<T>

--- src/main/kotlin/destiny/tools/converters/Coder.kt
+++ src/main/kotlin/destiny/tools/converters/Coder.kt
@@ -3,12 +3,28 @@
  */
 package destiny.tools.converters
 
+import destiny.tools.KotlinLogging
+
 interface Encoder<T> {
   fun encode(instance: T): String
 }
 
 interface Decoder<T> {
-  fun decode(encodedString : String) : T?
+
+  fun tryDecode(encodedString: String): T?
+
+  fun decode(encodedString : String) : T? {
+    return try {
+      tryDecode(encodedString)
+    } catch (e: Exception) {
+      logger.error(e) { "could not decode: ${encodedString.take(30)}...(len=${encodedString.length})" }
+      null
+    }
+  }
+
+  companion object {
+    private val logger = KotlinLogging.logger {  }
+  }
 }
 
 /** [Encoder] + [Decoder] */
